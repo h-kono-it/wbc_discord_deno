9e181f0542a270fe663eb42acd8eac72ca4393a9
hk <h.kono.it@gmail.com>
2023-03-17 23:13:46 +0900

    initial commit

---
+++ deps.ts
@@ -0,0 +1,4 @@
+// discordeno
+export * as discordeno from "https://deno.land/x/discordeno@17.0.1/mod.ts";
+// dotenv
+export * as dotenv from "https://deno.land/std@0.167.0/dotenv/mod.ts";
---
+++ main.ts
@@ -0,0 +1,99 @@
+import { discordeno } from "./deps.ts";
+import { Secret } from "./secret.ts";
+import { Members } from "./members.ts";
+
+const bot = discordeno.createBot({
+  token: Secret.DISCORD_TOKEN,
+  intents:
+    discordeno.Intents.Guilds |
+    discordeno.Intents.GuildMessages |
+    discordeno.Intents.MessageContent,
+  events: {
+    ready: (_bot, payload) => {
+      console.log(`${payload.user.username} is ready!`);
+    },
+  },
+});
+
+const listCommand: discordeno.CreateSlashApplicationCommand = {
+  name: "list",
+  description: "代表メンバーを返します",
+};
+const detailCommand: discordeno.CreateSlashApplicationCommand = {
+  name: "detail",
+  description: "代表メンバーの情報を返します",
+  options: [
+    {
+      type: discordeno.ApplicationCommandOptionTypes.String,
+      name: "number",
+      description: "背番号",
+      required: true,
+    },
+  ],
+};
+await bot.helpers.createGuildApplicationCommand(listCommand, Secret.GUILD_ID);
+await bot.helpers.upsertGuildApplicationCommands(Secret.GUILD_ID, [
+  listCommand,
+]);
+await bot.helpers.createGuildApplicationCommand(detailCommand, Secret.GUILD_ID);
+await bot.helpers.upsertGuildApplicationCommands(Secret.GUILD_ID, [
+  detailCommand,
+]);
+
+bot.events.messageCreate = (b, message) => {
+  if (message.content === "!neko") {
+    b.helpers.sendMessage(message.channelId, {
+      content: "にゃーん",
+    });
+  }
+};
+
+bot.events.interactionCreate = (b, interaction) => {
+  switch (interaction.data?.name) {
+    case "list": {
+      b.helpers.sendInteractionResponse(interaction.id, interaction.token, {
+        type: discordeno.InteractionResponseTypes.ChannelMessageWithSource,
+        data: {
+          content: "```" + JSON.stringify(Members) + "```",
+        },
+      });
+      break;
+    }
+    case "detail": {
+      b.helpers.sendInteractionResponse(interaction.id, interaction.token, {
+        type: discordeno.InteractionResponseTypes.ChannelMessageWithSource,
+        data: getMemberData(
+          (
+            interaction.data.options?.find((option) => option.name === "number")
+              ?.value || ""
+          ).toString()
+        ),
+      });
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+};
+
+const getMemberData = (
+  num: string
+): discordeno.InteractionCallbackData | undefined => {
+  const member = Members[num];
+  if (member) {
+    return {
+      content: member.name,
+      file: member.imageName
+        ? { blob: fileToBlob(member.imageName), name: member.imageName }
+        : undefined,
+    };
+  }
+  return undefined;
+};
+
+const fileToBlob = (path: string): Blob => {
+  return new Blob([Deno.readFileSync(`./images/${path}`).buffer]);
+};
+
+await discordeno.startBot(bot);
