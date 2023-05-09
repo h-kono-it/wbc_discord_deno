76d789a0656e78b5d01e2245e19c8848d939e366
hk <h.kono.it@gmail.com>
2023-05-09 15:49:25 +0900

    create list command

--- main.ts
+++ main.ts
@@ -32,12 +32,12 @@ const detailCommand: discordeno.CreateSlashApplicationCommand = {
     },
   ],
 };
-// await bot.helpers.createGuildApplicationCommand(listCommand, Secret.GUILD_ID);
+await bot.helpers.createGuildApplicationCommand(listCommand, Secret.GUILD_ID);
 // await bot.helpers.createGuildApplicationCommand(detailCommand, Secret.GUILD_ID);
-await bot.helpers.upsertGuildApplicationCommands(Secret.GUILD_ID, [
-  listCommand,
-  detailCommand,
-]);
+// await bot.helpers.upsertGuildApplicationCommands(Secret.GUILD_ID, [
+//   listCommand,
+//   detailCommand,
+// ]);
 
 bot.events.messageCreate = (b, message) => {
   if (message.content === "!neko") {
